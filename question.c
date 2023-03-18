7f2dbb3d5ad3b629823a947e77edb4d9ea7bfed6
TOHID RUPOK <88036943+tohidrupok@users.noreply.github.com>
2023-03-19 02:36:17 +0600

    Question of this Solutions || DIU_CSE 2nd Semester

---
+++ question.c
@@ -0,0 +1,85 @@
+#include<stdio.h>
+int main()
+{
+
+
+/*LAB Final Suggestion
+1. Write a C program to calculate days into year, month, days.
+Sample Input: 397 days
+Sample Output: 1 Year 1 Month 2 Days.
+2. Write a C program to check a year is leap year or not.
+Sample Input: 2100
+Sample Output: 2100 is not leap year.
+3. Write a C program to print the following pattern.
+*
+* *
+* * *
+* * * *
+* * * * *
+4. Write a C program to print the following pattern.
+1
+1 2
+1 2 3
+1 2 3 4
+1 2 3 4 5
+5. Write a C program to print the following pattern.
+1
+2 2
+3 3 3
+4 4 4 4
+5 5 5 5 5
+6. Write a C program to print the following pattern.
+1
+2 3
+4 5 6
+7 8 9 10
+11 12 13 14 15
+7. Write a program to check a number is prime or not.
+
+ Sample Input: 23
+Sample Output: 23 is not prime number.
+8. Write a program to check a number is perfect or not.
+Sample Input: 6
+Sample Output: 6 is a perfect number.
+9. Write a program to check a number is palindrome or not.
+Sample Input: 121
+Sample Output: 121 is a palindrome number.
+10. Write a program to calculate sum of a digit.
+Sample Input: 1214
+Sample Output: Sum = 8.
+11. Write a program to find the maximum number of an array.
+Sample Input: [12, 4, 56, 45, 20]
+Sample Output: Sum = Maximum number is 56.
+12. Write a program for sorting an array using Bubble Sort.
+Sample Input: [12, 4, 56, 45, 20]
+Sample Output: Sum = [4, 12, 20, 45, 56]
+13. Write a program for searching a value of an array using Linear Search.
+
+Sample Input: [12, 4, 56, 45, 20]
+Searching Value: 56
+Sample Output: Searching Value at Location [2].
+14. Write a program for searching a value of an array using Binary Search.
+
+Sample Input: [12, 4, 56, 45, 20]
+Searching Value: 56
+Sample Output: Searching Value at Location [2].
+15. Write a program to find the minimum number of an array.
+Sample Input: [12, 4, 56, 45, 20]
+Sample Output: Sum = Minimum number is 4.
+16. Write a c program to calculate area of a triangle using Function.
+Sample Input: a = 5, b = 2.
+Sample Output: area = 5.
+17. Write a c program to calculate factorial of a number using Recursive Function.
+Sample Input: n = 4.
+Sample Output: fact = 24.
+18. Write a c program to compute XY using Recursive Function.
+Sample Input: x = 5, y = 2.
+Sample Output: Value = 25.
+19. Write a c program to convert lower case into upper case of a string.
+Sample Input: diucse.
+Sample Output: DIUCSE.
+20. Write a c program to check a string is palindrome or not.
+Sample Input: madam.
+Sample Output: madam is palindrome.*/
+return 0;
+}
